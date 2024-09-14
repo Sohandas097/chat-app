295c9383195beb275092ddc6c34512efbf9026e9
Sohan Das <www.sohandas097@gmail.com>
2024-09-15 00:09:37 +0530

    initial project setup

---
+++ backend/db/connectToMongoDB.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const connectToMongoDB = async () => {
+    try {
+        
+    } catch (error) {
+        console.log("Error connecting to MongoDB",error.message);
+        
+    }
+}
