9ea3a04e62896d9008d1fad71e4dbcf6e0d88c08
Sohan Das <www.sohandas097@gmail.com>
2024-09-23 23:01:16 +0530

    all signup login logout endpoints added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,14 +1,93 @@
-export const signup = async (req,res) => {
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs"
+import generateTokenAndSetCookie from "../utils/generateToken.js";
+
+
+export const signup = async (req, res) => {
     try {
-        const {fullname,username,password,confirmPassword,gender} = req.body
+        const { fullName, username, password, confirmPassword, gender } = req.body;
+
+        if (password !== confirmPassword) {
+            return res.status(400).json({ error: "Password is not correct" })
+        }
+
+        const user = await User.findOne({ username }) //will check if this username is there or not
+
+        if (user) {
+            return res.status(400).json({ error: "Username already exists" })
+        }
+
+        //Hash password here
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        //https://avatar-placeholder.iran.liara.run/
+
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+        const newUser = new User({
+            fullName,
+            username,
+            password: hashedPassword,
+            gender,
+            profilePic: gender === "male" ? boyProfilePic : girlProfilePic
+        })
+
+        if (newUser) {
+            //Generate JWT tokens here
+            generateTokenAndSetCookie(newUser._id,res);
+
+            await newUser.save() // save this details of new user into database
+
+            res.status(201).json({ // created successfully
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic
+            })
+        } else {
+            res.status(400).json({ error: "Invalid user data" })
+        }
+
+
     } catch (error) {
-        
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ error: "Internal Server Error" })
     }
 }
-export const login = (req,res) => {
-    console.log("loginUser");
+export const login = async (req, res) => {
+    try {
+        const {username,password} = req.body;
+        const user = await User.findOne({username});
+        const isPasswordCorrect = await bcrypt.compare(password,user?.password || ""); // if the user exist or not
+
+        if(!user || !isPasswordCorrect){
+            return res.status(400).json({error:"Invalid username or password"})
+        }
+
+        generateTokenAndSetCookie(user._id,res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic,
+        })
+
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({ error: "Internal Server Error" })
+    }
 }
 
-export const logout = (req,res) => {
-    console.log("logoutUser");
+export const logout = (req, res) => {
+    try {
+        res.cookie("jwt","", {maxAge:0})
+        res.status(200).json({message:"Logged out successfully"})
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({ error: "Internal Server Error" })
+    }
 }
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,27 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js"
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express()
+const PORT = process.env.PORT || 9000
 
 dotenv.config();
-const PORT = process.env.PORT || 9000
+
+app.use(express.json()) //to parse the incoming requests with JSON payloads (from req.body)
+
+app.use("/api/auth",authRoutes)
+app.use("/api/messages",messageRoutes)
 
 app.get("/",(req,res) => {
     // root route http://localhost:7000/
     res.send("Hello World")
 })
 
-app.use("/api/auth",authRoutes)
 
 app.listen(PORT, () => {
+    console.log(PORT);
+    connectToMongoDB() //connecting to database
     console.log(`Server is running on port ${PORT}`);
 })
